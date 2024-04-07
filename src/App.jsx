97af7dedc1f58c5edb9f593a4a098666981c4d57
dylantopg <dylanjose619@udenar.edu.co>
2024-04-07 15:01:31 -0500

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import {
-  TextField,
-  RadioGroup,
-  Radio,
-  FormControlLabel,
-  Button,
-  Box,
-} from "@mui/material";
-
+import { useState, useEffect } from "react";
+import { TextField, RadioGroup, Radio, FormControlLabel, Button, Box } from "@mui/material";
 import * as Yup from "yup";
 import { Formik, Form } from "formik";
+import { db } from "./firebase"; // Assuming you have a Firebase configuration set up
 
 export default function App() {
+  const [nombreMonitor, setNombreMonitor] = useState("");
+  const [accion, setAccion] = useState("");
+  const [unidades, setUnidades] = useState(0);
+  const [comentario, setComentario] = useState("");
+
   const formSchema = Yup.object().shape({
-    nombreMonitor: Yup.string()
-      .max(255, "Máximo 255 caracteres")
-      .required("Campo requerido"),
+    nombreMonitor: Yup.string().max(255, "Máximo 255 caracteres").required("Campo requerido"),
     accion: Yup.string().required("Campo requerido"),
     unidades: Yup.number().positive().integer().required(),
     comentario: Yup.string(),
   });
 
+  const addRegistro = async (e) => {
+    e.preventDefault();
+
+    try {
+      await db.collection("registros").add({
+        nombreMonitor,
+        accion,
+        unidades,
+        comentario,
+      });
+      console.log("Data added to Firestore");
+    } catch (error) {
+      console.error("Error adding data to Firestore:", error);
+    }
+  };
+
   return (
     <>
       <h1>Registro de actividades</h1>
       <Formik
-        initialValues={{
-          nombreMonitor: "",
-          accion: "",
-          unidades: "",
-          comentario: "",
-        }}
+        initialValues={{ nombreMonitor: "", accion: "", unidades: "", comentario: "" }}
         validationSchema={formSchema}
         onSubmit={(values) => console.log(values)}
       >
@@ -41,6 +49,8 @@ export default function App() {
             margin="normal"
             type="text"
             fullWidth
+            value={nombreMonitor}
+            onChange={(e) => setNombreMonitor(e.target.value)}
           />
           <RadioGroup>
             <FormControlLabel
@@ -48,12 +58,16 @@ export default function App() {
               value="ingreso"
               control={<Radio />}
               label="Ingreso"
+              checked={accion === "ingreso"}
+              onChange={(e) => setAccion(e.target.value)}
             />
             <FormControlLabel
               name="accion"
               value="salida"
               control={<Radio />}
               label="Salida"
+              checked={accion === "salida"}
+              onChange={(e) => setAccion(e.target.value)}
             />
           </RadioGroup>
           <TextField
@@ -62,6 +76,8 @@ export default function App() {
             label="Unidades intervenidas"
             type="number"
             fullWidth
+            value={unidades}
+            onChange={(e) => setUnidades(parseInt(e.target.value))}
           />
           <TextField
             id="outlined-multiline-flexible"
@@ -70,14 +86,11 @@ export default function App() {
             minRows={4}
             maxRows={4}
             fullWidth
+            value={comentario}
+            onChange={(e) => setComentario(e.target.value)}
           />
           <Box textAlign="center">
-            <Button
-              variant="contained"
-              onClick={() => {
-                alert("clicked");
-              }}
-            >
+            <Button variant="contained" onClick={addRegistro}>
               Registrar!
             </Button>
           </Box>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4nJCnsfEGqAiJdWrgYl6II_plAvUWdR4",
+  authDomain: "monitores-be57a.firebaseapp.com",
+  projectId: "monitores-be57a",
+  storageBucket: "monitores-be57a.appspot.com",
+  messagingSenderId: "186490672517",
+  appId: "1:186490672517:web:d093e6299b67895566a0ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
